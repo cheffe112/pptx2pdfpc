e0382dc3f4fb56b33d82b26bb35ab2131c178e19
bytinbit <meline.sieber@ost.ch>
2021-10-06 17:01:03 +0200

    add options

--- pptx2pdfpc/cli.py
+++ pptx2pdfpc/cli.py
@@ -3,8 +3,16 @@ import sys
 
 import click
 
-
+from pptx2pdfpc.errors import Error
 from pptx2pdfpc.extract import speaker_notes, generate_pdfpc, generate_output_path
+
+
+
+# Options in pdfpc are not fully documented, but there's open issues and PRs concerning this:
+# https://github.com/pdfpc/pdfpc/issues/605
+# https://json-schema.app/view/%23?url=https%3A%2F%2Fraw.githubusercontent.com%2Fpdfpc%2Fpdfpc%2Fedb06b2c07e10a9210d4f1ae5861d2e847ca5065%2Fschema%2Fpdfpc.json
+
+
 
 
 @click.command()
@@ -18,19 +26,62 @@ from pptx2pdfpc.extract import speaker_notes, generate_pdfpc, generate_output_pa
     metavar="pt",
 )
 
+@click.option(
+    "--duration",
+    "-d",
+    "duration",
+    nargs=1,
+    type=click.INT,
+    help="Duration in minutes of the presentation used for timer display.",
+    metavar="minutes",
+)
+@click.option(
+    "--start-time",
+    "-t",
+    "start_time",
+    nargs=1,
+    type=click.STRING,
+    help="Start time of the presentation to be used as a countdown. (Format: HH:MM (24h))",
+    metavar="hh:mm",
+)
+@click.option(
+    "--end-time",
+    "-e",
+    "end_time",
+    nargs=1,
+    type=click.STRING,
+    help="End time of the presentation. (Format: HH:MM (24h))",
+    metavar="hh:mm",
+)
+@click.option(
+    "--last-minutes",
+    "-l",
+    "last_minutes",
+    nargs=1,
+    type=click.INT,
+    help="Time in minutes, from which on the timer changes its color. (Default: 5 minutes)",
+    metavar="mins",
+)
+@click.option(
+    "--end-slide",
+    "-s",
+    "end_slide",
+    nargs=1,
+    type=click.INT,
+    help="Set the last slide of the presentation.",
+    metavar="mins",
+)
 @click.argument("input_path", type=click.STRING)
-def main(input_path, font_size):
+def main(input_path, font_size, duration, start_time, end_time, last_minutes, end_slide):
+    options = [("font_size", font_size), ("duration", duration), ("start_time", start_time), ("end_time", end_time), ("last_minutes", last_minutes), ("end_slide", end_slide)]
     try:
         pptx = pathlib.Path(input_path)
         notes = speaker_notes(pptx)
-    except OSError as e:
-        print(f"An error happened while reading in the pptx:\n{e}")
+    except Error as e:
+        print(e)
         sys.exit(1)
 
     output_path = generate_output_path(pptx)
 
-    try:
-        generate_pdfpc(notes, output_path, font_size)
-    except OSError as e:
-        print(f"An error happened while generating the pdfpc notes in the pptx:\n{e}")
-        sys.exit(1)
+    generate_pdfpc(notes, options, output_path)
+    print(f"The notes have been extracted and can be found here:\n{output_path.name}")
--- pptx2pdfpc/extract.py
+++ pptx2pdfpc/extract.py
@@ -1,17 +1,22 @@
 import pathlib
-import pprint
 from typing import Tuple, List, Any
 
 from pptx import Presentation
+from pptx.exc import PackageNotFoundError
 
 # logic for extracting speaker notes and text boxes by user fusion on Stack Overflow, see:
 # https://stackoverflow.com/questions/63659972/extract-presenter-notes-from-pptx-file-powerpoint
 # Many thanks <3
+from pptx2pdfpc.errors import UsageError
 
 
 def speaker_notes(input_pptx: pathlib.Path) -> List[Tuple[int, str]]:
     """Extract all speaker notes from a pptx."""
-    prs = Presentation(str(input_pptx))
+    try:
+        prs = Presentation(str(input_pptx))
+    except PackageNotFoundError as e:
+        raise UsageError(f"There was an error while reading in the pptx:\n{e}")
+
     extracted = []
     for page, slide in enumerate(prs.slides, start=1):
         text = slide.notes_slide.notes_text_frame.text
@@ -32,7 +37,7 @@ def text_boxes(input_pptx: pathlib.Path) -> List[Tuple[int, List[Any]]]:
     return extracted
 
 
-def generate_pdfpc(extracted_notes: List[Tuple[int, str]], output_path: pathlib.Path, font_size: str=None):
+def generate_pdfpc(extracted_notes: List[Tuple[int, str]], options: List[Tuple[str, Any]], output_path: pathlib.Path, font_size: str=None):
     """Generate a config file for pdfpc and writes it to output_path.
     The file ending is .pdfpc and can be used together with a PDF
     version of the pptx presentation.
@@ -42,12 +47,11 @@ def generate_pdfpc(extracted_notes: List[Tuple[int, str]], output_path: pathlib.
     Requirement: Both files need to have the same name, one ending in .pdf, the other in .pdfpc.
     For pdfpc, see its man page or https://man.archlinux.org/man/community/pdfpc/pdfpc.1.en
     """
+    options_to_write = _create_options(options)
 
     DELIMITER = "###"
     with output_path.open("a") as fo:
-        if font_size:
-            fo.write("[font_size]\n")
-            fo.write(f"{font_size}\n")
+        fo.writelines(options_to_write)
         fo.write("[notes]\n")
         for slide in extracted_notes:
             page_number = slide[0]
@@ -65,3 +69,31 @@ def generate_output_path(input_path: pathlib.Path) -> pathlib.Path:
     filename = input_path.stem
     path = input_path.parent
     return path / pathlib.Path(filename + ".pdfpc")
+
+
+def _create_options(options: List[Tuple[str, Any]]) -> List[str]:
+    """Parse cli options and convert them to .pdfpc options
+
+    A list of available options .pdfpc options can be found here:
+    https://github.com/pdfpc/pdfpc/pull/556#issuecomment-739565800
+    duration 			 -d, --duration=N           Duration in minutes of the presentation used for timer  display.
+    start_time 		 -t, --start-time=T         Start time of the presentation to be used as a countdown.  (Format: HH:MM (24h))
+    end_time 			 -e, --end-time=T           End time of the presentation. (Format: HH:MM (24h))
+    last_minutes 	5 	 -l, --last-minutes=N       Time in minutes, from which on the timer changes its color. (Default: 5 minutes)
+    notes_position 	 -p  --notes-position       Position  of notes on the PDF page. Position can be either left, right, top or  bottom.
+    default_transition -a --default-transition
+    disable_markdown 	 -m --disable-markdown      FALSE
+    end_slide 		 -s --end-slide             Int.
+    last_saved_slide 	 -i --last-saved-slide
+    font_size 		 -f --font-size             Font size in pt. Int.
+    notes 			 -n --notes
+    forced_overlays 	 -v --forced-overlays
+    the options in pptx2pdfpc follow the command-line options in pdfpc if available
+    """
+
+    options_to_write = []
+    for option in options:
+        if option[1]:
+            options_to_write.append(f"[{option[0]}]\n{option[1]}\n")
+
+    return options_to_write
